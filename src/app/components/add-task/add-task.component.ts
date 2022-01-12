71e51b4351daa43e1ec563b56d5ef4570c4f39e9
Manakal <dp.manakal82@gmail.com>
2022-01-12 10:37:01 +0530

    Added Collapse and expand feature for Add Button

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Output,EventEmitter} from '@angular/core';
 import {ITask} from "../../ITask";
+import{UiService} from "../../services/ui.service";
+import {Subscription} from "rxjs";
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -10,8 +12,15 @@ export class AddTaskComponent implements OnInit {
   text:string;
   day:string;
   reminder:boolean=false;
-
-  constructor() { }
+  showAddTask:boolean;
+  subscription:Subscription;
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value:boolean) => {
+          this.showAddTask=value
+      })
+  }
 
   ngOnInit(): void {
   }
